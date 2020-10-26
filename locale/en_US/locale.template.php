8070bdd55f9b8e353f63a19116c7457a46f0b90d
pfeifferch <73090220+pfeifferch@users.noreply.github.com>
2020-10-26 10:00:49 +0100

    Create locale.template.php

---
+++ locale/en_US/locale.template.php
@@ -0,0 +1,54 @@
+<?php 
+//// Debug Message
+//echo "Englische Sprachdatei geladen"; 
+
+
+$setHTMLLanguageCode = "en";
+
+$config['localeHowManyMailArrivedBevore']  = "There are "; //Counter
+$config['localeMailHaveBeArrivedAfter']  = " new Mails.";
+
+$config['localeMailArrived']  = "<strong>New emails</strong> have arrived";
+
+$config['localeTranslationForRefresh']  = "Reload!";
+
+$config['localeMailboxReady']  =  "Your disposable mailbox is ready.";
+
+$config['localeTranslationforCopy']  =  "copy";
+
+$config['localeChangeMailUsername']  = "Change address";
+
+$config['localeSetToRandom']  = "Open random mailbox";
+
+$config['localeUseOwnUsername']  = "or create your own address:";
+
+$config['localeTranslationForDomain']  = "Domain";
+
+$config['localeOpenMailbox']  = "Open mailbox";
+
+$config['localeTranslationForDownload']  =  "Download";
+
+$config['localeTranslationForDelete']  =  "Delete";
+
+$config['localeEmptyMailbox']  = "<p>Emails will appear here automatically. </p>";
+
+$config['localeLanguage1']  = "English";
+
+$config['localeLanguage2']  = "German";
+
+$config['localeLanguage3']  = "Spanish";
+
+$config['localeQuickSummary']  = "This is a disposable mailbox service. <br/>Whoever knows your username, can read your emails.<br/>&nbsp;<br/>Emails will be deleted after ".$config['delete_messages_older_than']."days.";
+
+$config['localecollapse']  = "Show Details";
+
+$config['localelong-about-1']  = "<p class=\"text-justify\">This disposable mailbox keeps your main mailbox clean from spam.</p>";
+
+$config['localelong-about-2']  = "<p class=\"text-justify\">you don't trust and don't  want to use your main email address.<br/>
+Once you are done, you can just forget about the mailbox. <br/>All the spam stays here and does not fill up your main mailbox.</p>";
+
+$config['localelong-about-3']  = "<p class=\"text-justify\">You select the address you want to use and received emails will be displayed automatically. <br/>There is not registration and no passwords. If you know the address, you can read the emails. <br/><strong>Basically, all emails are public. So don't use it for sensitive data.</strong></p>";
+
+$config['localeCopyright']  = "<small><a href=\"https://github.com/pfeifferch/disposable-mailbox\"><strong>disposable-mailbox</strong></a> V2.1.1_EN - a Fork of <a href=\"https://github.com/synox/disposable-mailbox\"><strong>synox</strong></a></small>";
+
+?>
