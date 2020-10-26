a6e6d54d3117e6a0fdf2c07669864c40ed0f25d1
pfeifferch <73090220+pfeifferch@users.noreply.github.com>
2020-10-26 09:59:28 +0100

    Create locale.template.php

---
+++ locale/de_DE/locale.template.php
@@ -0,0 +1,52 @@
+<?php 
+//// Debug Message
+//echo "Deutsche Sprachdatei geladen"; 
+
+
+$setHTMLLanguageCode = "de";
+
+$config['localeHowManyMailArrivedBevore']  = "es gibt "; //Counter
+$config['localeMailHaveBeArrivedAfter']  = " neue Mails.";
+
+$config['localeMailArrived']  = "<strong>neue eMails</strong> sind eingetroffen.";
+
+$config['localeTranslationForRefresh']  = "Aktualisieren!";
+
+$config['localeMailboxReady']  =  "Deine Einweg-Mailadresse ist bereit.";
+
+$config['localeTranslationforCopy']  =  "Kopieren";
+
+$config['localeChangeMailUsername']  = "Addresse wechseln";
+
+$config['localeSetToRandom']  = "Zuf&auml;llige Mailadresse generieren";
+
+$config['localeUseOwnUsername']  = "oder eigene Adresse anlegen:";
+
+$config['localeTranslationForDomain']  = "Domain";
+
+$config['localeOpenMailbox']  = "&Ouml;ffne Mailbox";
+
+$config['localeTranslationForDownload']  =  "Download";
+
+$config['localeTranslationForDelete']  =  "L&ouml;schen";
+
+$config['localeEmptyMailbox']  = "<p>Das Postfach ist leer. Solange diese Seite ge&ouml;ffnet ist, wird Automatisch nach neuen E-Mails gesucht...</p>";
+
+$config['localeLanguage1']  = "Englisch";
+
+$config['localeLanguage2']  = "Deutsch";
+
+$config['localeLanguage3']  = "Spanisch";
+
+$config['localeQuickSummary']  = "Dies ist ein Einweg-Postfachdienst.  Wer Ihren Benutzernamen kennt, kann Ihre E-Mails lesen. <br/>Emails werden automatisch und unwiederbringlich gel&ouml;scht nach ". $config['delete_messages_older_than']." Tagen.";
+
+$config['localecollapse']  = "Mehr Infos";
+
+$config['localelong-about-1']  = "<p class=\"text-justify\">Diese Einweg-Mailbox h&auml;lt Ihre Hauptmailbox frei von Spam.</p>";
+
+$config['localelong-about-2']  = "<p class=\"text-justify\">W&auml;hlen Sie einfach eine Adresse und verwenden Sie sie auf Websites, denen Sie nicht vertrauen und wo Sie die Private Haupt-E-Mail-Adresse nicht preisgeben wollen. <br/>Sobald Sie fertig sind, können Sie die Mailbox einfach vergessen. <br/> Der ganze Spam bleibt hier wird nicht im Privaten Mailaccount landen.</p>";
+
+$config['localelong-about-3']  = "<p class=\"text-justify\">Sie wählen die Adresse aus, die Sie verwenden m&ouml;chten, und empfangene E-Mails werden automatisch angezeigt. <br/>Es gibt keine Registrierung und keine Passwörter. <br/>Wenn Sie die Adresse kennen, können Sie die E-Mails lesen.<br/><br/>             <strong>Grunds&auml;tzlich sind alle E-Mails &ouml;ffentlich. <br/>Verwenden Sie es also nicht für vertrauliche Daten.</strong></p>";
+
+$config['localeCopyright']  = "<small><a href=\"https://github.com/pfeifferch/disposable-mailbox\"><strong>disposable-mailbox</strong></a> V2.1.1_DE - a Fork of <a href=\"https://github.com/synox/disposable-mailbox\"><strong>synox</strong></a></small>";
+?>
