40b708b8849139598ac2298f73380ff11af8bb12
pfeifferch <73090220+pfeifferch@users.noreply.github.com>
2020-10-27 22:56:28 +0100

    Update sample.usercontent.php

--- sample.usercontent.php
+++ sample.usercontent.php
@@ -1,4 +1,4 @@
 <?php
 $usercontent['headline'] = "<h2 style=\"text-align:center;\">📧<i style=\"font-family:'Calligraffitti',sans-serif;font-weight:300\">Disposable Mailbox</i></h2><hr>"; 
-$usercontent['footer'] = ""; 
+$usercontent['footer'] = "<!-- <hr>Thank you 4 using DisposableMailbox --->"; 
 ?>
