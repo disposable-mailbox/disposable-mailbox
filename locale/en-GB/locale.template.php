f8f999158b16979e1d0867fe21f95347b04affa6
pfeifferch <73090220+pfeifferch@users.noreply.github.com>
2020-10-31 05:17:34 +0100

    Create locale.template.php

---
+++ locale/en-GB/locale.template.php
@@ -0,0 +1,51 @@
+<?php
+
+$setHTMLLanguageCode = "en";
+
+$locale['HowManyMailArrivedBevore']  = "There are "; //Counter
+$locale['MailHaveBeArrivedAfter']  = " new Mails.";
+
+$locale['MailArrived']  = "<strong>New emails</strong> have arrived";
+
+$locale['TranslationForRefresh']  = "Reload!";
+
+$locale['MailboxReady']  =  "Your disposable mailbox is ready.";
+
+$locale['TranslationforCopy']  =  "copy";
+
+$locale['ChangeMailUsername']  = "Change address";
+
+$locale['SetToRandom']  = "Open random mailbox";
+
+$locale['UseOwnUsername']  = "or create your own address:";
+
+$locale['TranslationForDomain']  = "Domain";
+
+$locale['OpenMailbox']  = "Open mailbox";
+
+$locale['TranslationForDownload']  =  "Download";
+
+$locale['TranslationForDelete']  =  "Delete";
+
+$locale['EmptyMailbox']  = "<p>Emails will appear here automatically. </p>";
+
+$locale['Language1']  = "English";
+
+$locale['Language2']  = "German";
+
+$locale['Language3']  = "Spanish";
+
+$locale['QuickSummary']  = "This is a disposable mailbox service. <br/>Whoever knows your username, can read your emails.<br/>&nbsp;<br/>Emails will be deleted after ".$config['delete_messages_older_than']."days.";
+
+$locale['collapse']  = "Show Details";
+
+$locale['long-about-1']  = "<p class=\"text-justify\">This disposable mailbox keeps your main mailbox clean from spam.</p>";
+
+$locale['long-about-2']  = "<p class=\"text-justify\">you don't trust and don't  want to use your main email address.<br/>
+Once you are done, you can just forget about the mailbox. <br/>All the spam stays here and does not fill up your main mailbox.</p>";
+
+$locale['long-about-3']  = "<p class=\"text-justify\">You select the address you want to use and received emails will be displayed automatically. <br/>There is not registration and no passwords. If you know the address, you can read the emails. <br/><strong>Basically, all emails are public. So don't use it for sensitive data.</strong></p>";
+
+$locale['Copyright']  = "<small><a href=\"https://github.com/pfeifferch/disposable-mailbox\"><strong>disposable-mailbox</strong></a> ".$config['versionnumber']."_EN - a detached Fork of <a href=\"https://github.com/synox/disposable-mailbox\"><strong>synox</strong></a></small>";
+
+?>
