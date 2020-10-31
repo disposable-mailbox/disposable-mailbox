00cb5b8c64e7553fe6c0a8fac54dca65354b3db3
pfeifferch <73090220+pfeifferch@users.noreply.github.com>
2020-10-31 04:32:10 +0100

    Update frontend.template.php

--- frontend.template.php
+++ frontend.template.php
@@ -57,6 +57,10 @@ Once you are done, you can just forget about the mailbox. <br/>All the spam stay
 $locale['long-about-3'] = "<p class=\"text-justify\">You select the address you want to use and received emails will be displayed automatically. <br/>There is not registration and no passwords. If you know the address, you can read the emails. <br/><strong>Basically, all emails are public. So don't use it for sensitive data.</strong></p>";
 $locale['Copyright'] = "<small><a href=\"https://github.com/pfeifferch/disposable-mailbox\"><strong>disposable-mailbox</strong></a> ".$config['versionnumber']."_NT - a detached Fork of <a href=\"https://github.com/synox/disposable-mailbox\"><strong>synox</strong></a></small>";
 }
+
+}
+
+
 // if empty
 if (empty($usercontent['headline'])) {$usercontent['headline'] = "<h2 style=\"text-align:center;\">📧<i style=\"font-family:'Calligraffitti',sans-serif;font-weight:300\">Disposable Mailbox</i></h2><hr>";}
 if (empty($usercontent['footer'])) {$usercontent['footer'] = "<!-- <hr>Thank you 4 using DisposableMailbox --->";} 
