17f6ed63751ff52d7f82de1323ef2214cb9b7844
pfeifferch <73090220+pfeifferch@users.noreply.github.com>
2020-10-31 14:01:53 +0100

    Update locale.template.php

--- locale/es/locale.template.php
+++ locale/es/locale.template.php
@@ -39,9 +39,11 @@ $locale['QuickSummary']  = "Este es un servicio de buzón unidireccional.  Quien
 
 $locale['collapse']  = "Más información";
 
-$locale['long-about-1']  = "<p class=\"text-justify\">Diese Einweg-Mailbox h&auml;lt Ihre Hauptmailbox frei von Spam.</p>";
+$locale['long-about-1']  = "<p class=\"text-justify\"Este buzón desechable mantendrá su buzón principal libre de spam.</p>";
 
-$locale['long-about-2']  = "<p class=\"text-justify\">W&auml;hlen Sie einfach eine Adresse und verwenden Sie sie auf Websites, denen Sie nicht vertrauen und wo Sie die Private Haupt-E-Mail-Adresse nicht preisgeben wollen. <br/>Sobald Sie fertig sind, können Sie die Mailbox einfach vergessen. <br/> Der ganze Spam bleibt hier wird nicht im Privaten Mailaccount landen.</p>";
+$locale['long-about-2']  = "<p class=\"text-justify\">Simplemente elija una dirección y utilícela en sitios web en los que no confíe y en los que no desee revelar la dirección de correo electrónico privada principal.
+ Una vez que haya terminado, puede simplemente olvidarse del buzón.
+ Todo el spam permanece aquí y no terminará en la cuenta de correo privado<br/>Sobald Sie fertig sind, können Sie die Mailbox einfach vergessen. <br/> Der ganze Spam bleibt hier wird nicht im Privaten Mailaccount landen.</p>";
 
 $locale['long-about-3']  = "<p class=\"text-justify\">Sie wählen die Adresse aus, die Sie verwenden m&ouml;chten, und empfangene E-Mails werden automatisch angezeigt. <br/>Es gibt keine Registrierung und keine Passwörter. <br/>Wenn Sie die Adresse kennen, können Sie die E-Mails lesen.<br/><br/>             <strong>Grunds&auml;tzlich sind alle E-Mails &ouml;ffentlich. <br/>Verwenden Sie es also nicht für vertrauliche Daten.</strong></p>";
 
