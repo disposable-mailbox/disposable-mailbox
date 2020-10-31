aaf495406028cdba048d3b032dc8f1aff5c472fc
pfeifferch <73090220+pfeifferch@users.noreply.github.com>
2020-10-31 04:12:27 +0100

    Update frontend.template.php

--- frontend.template.php
+++ frontend.template.php
@@ -92,6 +92,32 @@ function printMessageBody($email, $purifier) {
     }
 }
 
+
+/**
+ * Detect Browser Language
+ * (c) http://martin-mueller.info/2014/03/24/sprache-des-benutzers-oder-browsers-erkennen-php/
+ */
+function getPreferredLanguage(){
+ 
+	$acceptedLanguages = @explode(',', $_SERVER['HTTP_ACCEPT_LANGUAGE']);
+	$preferredLanguage = null;
+	$maxWeight = 0.0;
+ 
+	foreach((array)$acceptedLanguages as $acceptedLanguage){
+ 
+		$weight = (float)@substr(explode(';', $acceptedLanguage)[1], 2);
+		if(!$weight){$weight = 1.0;}
+ 
+		if($weight > $maxWeight){
+			$preferredLanguage =  substr($acceptedLanguage, 0, 2);  // evtl kürzen
+			$maxWeight = $weight;
+		}
+	}
+ 
+	return $preferredLanguage;
+}
+
+
 ?>
 
 
@@ -144,6 +170,7 @@ function printMessageBody($email, $purifier) {
 </head>
 <body>
 
+<?php echo "<!-- ".getPreferredLanguage()." -->"; ?>
 
 <div id="new-content-avalable">
     <div class="alert alert-info alert-fixed" role="alert">
