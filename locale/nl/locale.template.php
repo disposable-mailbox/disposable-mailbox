1dbffee7b4c18e9355b070674a693604da797766
pfeifferch <73090220+pfeifferch@users.noreply.github.com>
2020-11-30 13:43:34 +0100

    Update locale.template.php

--- locale/nl/locale.template.php
+++ locale/nl/locale.template.php
@@ -36,12 +36,9 @@ $locale['collapse']  = "Meer informatie";
 $locale['long-about-1']  = "<p class=\"text-justify\">Deze wegwerpmailbox houdt uw hoofdmailbox spamvrij.</p>";
 
 $locale['long-about-2']  = "<p class=\"text-justify\">
-W&auml;hlen Sie einfach eine Adresse und verwenden Sie sie auf Websites, 
-denen Sie nicht vertrauen und wo Sie die Private Haupt-E-Mail-Adresse nicht preisgeben wollen. <br/>
-
-Sobald Sie fertig sind, können Sie die Mailbox einfach vergessen. <br/> 
-
-Der ganze Spam bleibt hier wird nicht im Privaten Mailaccount landen.</p>";
+Kies gewoon een adres en gebruik het op websites die u niet vertrouwt en waar u het belangrijkste privé-e-mailadres niet wilt onthullen. <br/>
+Als je klaar bent, kun je de mailbox gewoon vergeten.<br/>
+Alle spam blijft hier en komt niet in het privé mailaccount terecht.</p>";
 
 $locale['long-about-3']  = "<p class=\"text-justify\">Sie wählen die Adresse aus, die Sie verwenden m&ouml;chten, und empfangene E-Mails werden automatisch angezeigt. <br/>Es gibt keine Registrierung und keine Passwörter. <br/>Wenn Sie die Adresse kennen, können Sie die E-Mails lesen.<br/><br/>             <strong>Grunds&auml;tzlich sind alle E-Mails &ouml;ffentlich. <br/>Verwenden Sie es also nicht für vertrauliche Daten.</strong></p>";
 
