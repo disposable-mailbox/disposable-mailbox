36a6a881021cb47680b2a994e853a000df0fe074
pfeifferch <73090220+pfeifferch@users.noreply.github.com>
2020-11-30 21:41:34 +0100

    Update sample.usercontent.php

--- sample.usercontent.php
+++ sample.usercontent.php
@@ -1,4 +1,14 @@
 <?php
-$usercontent['headline'] = "<h2 style=\"text-align:center;\">📧<i style=\"font-family:'Calligraffitti',sans-serif;font-weight:300\">Disposable Mailbox</i></h2><hr>"; 
-$usercontent['footer'] = "<!-- <hr>Thank you 4 using DisposableMailbox --->"; 
+//edit disposable-mailbox.eu to your Main Domain ;)
+$usercontent['headline'] = "<h2 style=\"text-align:center;\">📧 <i style=\"font-family:'Calligraffitti',sans-serif;font-weight:300\"><a href=\"https://www.disposable-mailbox.eu/?$user->username@".$user->domain."\">disposable mailbox</a></i></h2><hr>"; 
+
+// Translation assistance is requested. Standard is:
+//$usercontent['footer'] = "";
+if ($localeselected == 'nl') {
+$usercontent['footer'] = "<hr><blockquote>Deze vertaling is gemaakt met behulp van programma's.  help alstublieft op <a href=\"https://github.com/pfeifferch/disposable-mailbox\">GitHub</a> om een ​​begrijpelijke vertaling voor te bereiden.</blockquote>"; 
+} elseif ($localeselected == 'es') {
+$usercontent['footer'] = "<hr><blockquote>Esta traducción se creó con la ayuda de programas.  ayuda en <a href=\"https://github.com/pfeifferch/disposable-mailbox\">GitHub</a> para preparar una traducción comprensible.</blockquote>"; 
+} else    { 
+$usercontent['footer'] = "<br>"; }
+
 ?>
