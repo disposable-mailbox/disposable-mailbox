473e8f08b54e3b18ec143471361e50ad99b889e8
pfeifferch <73090220+pfeifferch@users.noreply.github.com>
2020-11-30 13:37:07 +0100

    Update locale.template.php

--- locale/nl/locale.template.php
+++ locale/nl/locale.template.php
@@ -33,14 +33,16 @@ $locale['QuickSummary']  = "Dit is een eenrichtings-e-mailservice.  Wie uw gebru
 
 $locale['collapse']  = "Meer informatie";
 
-$locale['long-about-1']  = "<p class=\"text-justify\">Diese Einweg-Mailbox h&auml;lt Ihre Hauptmailbox frei von Spam.</p>";
+$locale['long-about-1']  = "<p class=\"text-justify\">Deze wegwerpmailbox houdt uw hoofdmailbox spamvrij.</p>";
 
-$locale['long-about-2']  = "<p class=\"text-justify\">W&auml;hlen Sie einfach eine Adresse und verwenden Sie sie auf Websites, denen Sie nicht vertrauen und wo Sie die Private Haupt-E-Mail-Adresse nicht preisgeben wollen. <br/>Sobald Sie fertig sind, können Sie die Mailbox einfach vergessen. <br/> Der ganze Spam bleibt hier wird nicht im Privaten Mailaccount landen.</p>";
-
+$locale['long-about-2']  = "<p class=\"text-justify\">
+W&auml;hlen Sie einfach eine Adresse und verwenden Sie sie auf Websites, 
+denen Sie nicht vertrauen und wo Sie die Private Haupt-E-Mail-Adresse nicht preisgeben wollen. <br/>Sobald Sie fertig sind, können Sie die Mailbox einfach vergessen. <br/> Der ganze Spam bleibt hier wird nicht im Privaten Mailaccount landen.</p>";
+// $ locale ['long-about-2'] = "<p class = \" text-justify \ "> Kies gewoon een adres en gebruik het op websites die u niet vertrouwt en waar u de privé hoofd-e-  Wil je je e-mailadres niet prijsgeven. <br/> Zodra je klaar bent, kun je je mailbox gewoon vergeten. <br/> Alle spam die hier blijft, komt niet in je privé-mailaccount terecht. </p> ";
 $locale['long-about-3']  = "<p class=\"text-justify\">Sie wählen die Adresse aus, die Sie verwenden m&ouml;chten, und empfangene E-Mails werden automatisch angezeigt. <br/>Es gibt keine Registrierung und keine Passwörter. <br/>Wenn Sie die Adresse kennen, können Sie die E-Mails lesen.<br/><br/>             <strong>Grunds&auml;tzlich sind alle E-Mails &ouml;ffentlich. <br/>Verwenden Sie es also nicht für vertrauliche Daten.</strong></p>";
 
 $locale['Copyright']  = "<small><a href=\"https://github.com/pfeifferch/disposable-mailbox\"><strong>disposable-mailbox</strong></a> ".$config['versionnumber']."_NL - a detached Fork of <a href=\"https://github.com/synox/disposable-mailbox\"><strong>synox</strong></a></small>";
 
-$locale['ourdomains'] = "&Uuml;bersicht der verf&uuml;gbaren Domains";
+$locale['ourdomains'] = "Overzicht van de beschikbare domeinen";
 
 ?>
