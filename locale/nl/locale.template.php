30e8b033834b8810d425977e3eaf54bca0ff5d7f
pfeifferch <73090220+pfeifferch@users.noreply.github.com>
2020-12-04 22:48:26 +0100

    Update locale.template.php

--- locale/nl/locale.template.php
+++ locale/nl/locale.template.php
@@ -24,7 +24,6 @@ $locale['TranslationForDomain']  = "domein";
 $locale['OpenMailbox']  = "open mailbox";
 
 $locale['TranslationForDownload']  =  "Downloaden";
-
 $locale['TranslationForDelete']  =  "Doorzichtig";
 
 $locale['EmptyMailbox']  = "<p>De mailbox is leeg.  Zolang deze pagina open is, zoekt deze automatisch naar nieuwe e-mails ...</p>";
@@ -32,15 +31,16 @@ $locale['EmptyMailbox']  = "<p>De mailbox is leeg.  Zolang deze pagina open is, 
 $locale['QuickSummary']  = "Dit is een eenrichtings-e-mailservice.  Wie uw gebruikersnaam kent, kan uw e-mails lezen. E-mails worden automatisch en onherroepelijk verwijderd na ". $config['delete_messages_older_than']." dagen.";
 
 $locale['collapse']  = "Meer informatie";
-
 $locale['long-about-1']  = "<p class=\"text-justify\">Deze wegwerpmailbox houdt uw hoofdmailbox spamvrij.</p>";
-
 $locale['long-about-2']  = "<p class=\"text-justify\">Kies gewoon een adres en gebruik het op websites die u niet vertrouwt en waar u het belangrijkste privé-e-mailadres niet wilt onthullen. <br/>Als je klaar bent, kun je de mailbox gewoon vergeten.<br/>Alle spam blijft hier en komt niet in het privé mailaccount terecht.</p>";
-
 $locale['long-about-3']  = "<p class=\"text-justify\">U selecteert het adres dat u wilt gebruiken en ontvangen e-mails verschijnen automatisch <br/>Er is geen registratie en geen wachtwoorden. <br/>Als u het adres kent, kunt u de e-mails lezen.<br/><br/>             <strong>In principe zijn alle e-mails openbaar.<br/>Gebruik het dus niet voor gevoelige gegevens.</strong></p>";
 
-$locale['Copyright']  = "<small><a href=\"https://github.com/pfeifferch/disposable-mailbox\"><strong>disposable-mailbox</strong></a> ".$config['versionnumber']."_NL - een vrijstaande vork van <a href=\"https://github.com/synox/disposable-mailbox\"><strong>synox</strong></a></small>";
-
 $locale['ourdomains'] = "Overzicht van de beschikbare domeinen";
+
+$locale['success'] = "Met succes";
+$locale['copied'] = "Gekopieerd!";
+$locale['show'] = "Tonen";
+
+$locale['Copyright']  = "<small><a href=\"https://github.com/pfeifferch/disposable-mailbox\"><strong>disposable-mailbox</strong></a> ".$config['versionnumber']."_NL - een vrijstaande vork van <a href=\"https://github.com/synox/disposable-mailbox\"><strong>synox</strong></a></small>";
 
 ?>
