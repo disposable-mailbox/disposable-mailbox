88de95fbf89e6d529e8d34268003da50d89e54e5
pfeifferch <73090220+pfeifferch@users.noreply.github.com>
2020-12-04 22:41:20 +0100

    Update locale.template.php

--- locale/de_DE/locale.template.php
+++ locale/de_DE/locale.template.php
@@ -14,9 +14,7 @@ $locale['MailboxReady']  =  "Deine Einweg-Mailadresse ist bereit.";
 $locale['TranslationforCopy']  =  "Kopieren";
 
 $locale['ChangeMailUsername']  = "Adresse wechseln";
-
 $locale['SetToRandom']  = "Zuf&auml;llige Mailadresse generieren";
-
 $locale['UseOwnUsername']  = "oder eigene Adresse anlegen:";
 
 $locale['TranslationForDomain']  = "Domain";
@@ -24,26 +22,21 @@ $locale['TranslationForDomain']  = "Domain";
 $locale['OpenMailbox']  = "&Ouml;ffne Mailbox";
 
 $locale['TranslationForDownload']  =  "Download";
-
 $locale['TranslationForDelete']  =  "L&ouml;schen";
 
 $locale['EmptyMailbox']  = "<p>Das Postfach ist leer. Solange diese Seite ge&ouml;ffnet ist, wird Automatisch nach neuen E-Mails gesucht...</p>";
 
 $locale['QuickSummary']  = "Dies ist ein Einweg-Postfachdienst.  Wer Ihren Benutzernamen kennt, kann Ihre E-Mails lesen. <br/>Emails werden automatisch und unwiederbringlich gel&ouml;scht nach ". $config['delete_messages_older_than']." Tagen.";
-
 $locale['collapse']  = "Mehr Infos";
-
 $locale['long-about-1']  = "<p class=\"text-justify\">Diese Einweg-Mailbox h&auml;lt Ihre Hauptmailbox frei von Spam.</p>";
-
 $locale['long-about-2']  = "<p class=\"text-justify\">W&auml;hlen Sie einfach eine Adresse und verwenden Sie sie auf Websites, denen Sie nicht vertrauen und wo Sie die Private Haupt-E-Mail-Adresse nicht preisgeben wollen. <br/>Sobald Sie fertig sind, können Sie die Mailbox einfach vergessen. <br/> Der ganze Spam bleibt hier wird nicht im Privaten Mailaccount landen.</p>";
-
 $locale['long-about-3']  = "<p class=\"text-justify\">Sie wählen die Adresse aus, die Sie verwenden m&ouml;chten, und empfangene E-Mails werden automatisch angezeigt. <br/>Es gibt keine Registrierung und keine Passwörter. <br/>Wenn Sie die Adresse kennen, können Sie die E-Mails lesen.<br/><br/>             <strong>Grunds&auml;tzlich sind alle E-Mails &ouml;ffentlich. <br/>Verwenden Sie es also nicht für vertrauliche Daten.</strong></p>";
 
-$locale['ourdomains'] = "Our Domains";
+$locale['ourdomains'] = "&Uuml;bersicht der verf&Uuml;gbaren Domains";
 
-$locale['success'] = "success";
-$locale['copied'] = "Copied!";
-$locale['show'] = "show";
+$locale['success'] = "Erfolgreich";
+$locale['copied'] = "Kopiert!";
+$locale['show'] = "Anzeigen";
 
 $locale['Copyright']  = "<small><a href=\"https://github.com/pfeifferch/disposable-mailbox\"><strong>disposable-mailbox</strong></a> ".$config['versionnumber']."_DE-DE - eine Weiterentwicklung von <a href=\"https://github.com/synox/disposable-mailbox\"><strong>synox</strong></a></small>";
 
