1d99fe2fe11c6886084a017c077812ebe8c2233b
pfeifferch <73090220+pfeifferch@users.noreply.github.com>
2020-12-04 19:24:12 +0100

    Update frontend.template.php

--- frontend.template.php
+++ frontend.template.php
@@ -42,11 +42,12 @@ $locale['collapse'] = "Show Details";
 $locale['long-about-2'] = "<p class=\"text-justify\">you don't trust and don't want to use your main email address.<br/>
 Once you are done, you can just forget about the mailbox. <br/>All the spam stays here and does not fill up your main mailbox.</p>";
 $locale['long-about-3'] = "<p class=\"text-justify\">You select the address you want to use and received emails will be displayed automatically. <br/>There is not registration and no passwords. If you know the address, you can read the emails. <br/><strong>Basically, all emails are public. So don't use it for sensitive data.</strong></p>";
-$locale['Copyright'] = "<small><a href=\"https://github.com/pfeifferch/disposable-mailbox\"><strong>disposable-mailbox</strong></a> ".$config['versionnumber']."_NT - a detached Fork of <a href=\"https://github.com/synox/disposable-mailbox\"><strong>synox</strong></a></small>";
 $locale['ourdomains'] = "Our Domains";
-$locale['success']; = "success";
+$locale['success'] = "success";
 $locale['copied'] = "Copied!";
-$locale['show']; = "show";
+$locale['show'] = "show";
+$locale['Copyright'] = "<small><a href=\"https://github.com/pfeifferch/disposable-mailbox\"><strong>disposable-mailbox</strong></a> ".$config['versionnumber']."_NT - a detached Fork of <a href=\"https://github.com/synox/disposable-mailbox\"><strong>synox</strong></a></small>";
+
 }
 }
 }
