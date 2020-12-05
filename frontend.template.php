75c1815615459a7b1b5f8aec921284cb2cbdeb7e
pfeifferch <73090220+pfeifferch@users.noreply.github.com>
2020-12-05 01:17:35 +0100

    Update frontend.template.php

--- frontend.template.php
+++ frontend.template.php
@@ -455,7 +455,7 @@ $('[data-tooltip="tooltip"]').tooltip()
 /** from https://github.com/twbs/bootstrap/blob/c11132351e3e434f6d4ed72e5a418eb692c6a319/assets/js/src/application.js */
 clipboard.on('success', function (e) {
 $(e.trigger)
-.attr('title', '<?php echo $locale['copied']; ?>!')
+.attr('title', '<?php echo $locale['copied']; ?>')
 .tooltip('_fixTitle')
 .tooltip('show')
 .tooltip('_fixTitle');
