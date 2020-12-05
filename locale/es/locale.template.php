3891ccd7b0008eb22e63b5ff203ac45a29240d41
pfeifferch <73090220+pfeifferch@users.noreply.github.com>
2020-12-05 03:25:42 +0100

    Update locale.template.php

--- locale/es/locale.template.php
+++ locale/es/locale.template.php
@@ -14,9 +14,7 @@ $locale['MailboxReady']  =  "Su dirección de correo electrónico unidireccional
 $locale['TranslationforCopy']  =  "Copiar";
 
 $locale['ChangeMailUsername']  = "Cambiar dirección";
-
 $locale['SetToRandom']  = "Genere una dirección de correo electrónico aleatoria";
-
 $locale['UseOwnUsername']  = "oo crea tu propia dirección:";
 
 $locale['TranslationForDomain']  = "dominio";
@@ -24,7 +22,6 @@ $locale['TranslationForDomain']  = "dominio";
 $locale['OpenMailbox']  = "buzón abierto";
 
 $locale['TranslationForDownload']  =  "Descargar";
-
 $locale['TranslationForDelete']  =  "Claro";
 
 $locale['EmptyMailbox']  = "<p>El buzón está vacío. Mientras esta página esté abierta, buscará automáticamente nuevos correos electrónicos ... </p>";
@@ -34,14 +31,11 @@ Quien conozca su nombre de usuario puede leer sus correos electrónicos.  <br/>
 Los correos electrónicos se eliminan automática e irremediablemente después de ". $config['delete_messages_older_than']."  Dias. ";
 
 $locale['collapse']  = "Más información";
-
 $locale['long-about-1']  = "<p class=\"text-justify\"Este buzón desechable mantendrá su buzón principal libre de spam.</p>";
-
 $locale['long-about-2']  = "<p class=\"text-justify\">Simplemente elija una dirección y utilícela en sitios web en los que no confíe y en los que no 
 desee revelar la dirección de correo electrónico privada principal.<br/>
 Una vez que haya terminado, puede simplemente olvidarse del buzón.<br/>
 Todo el spam permanece aquí y no terminará en la cuenta de correo Private.</p>";
-
 $locale['long-about-3']  = "<p class=\"text-justify\">
 Selecciona la dirección que desea usar y los correos electrónicos recibidos aparecerán automáticamente.<br/>
 No hay registro ni contraseñas.<br/>
@@ -49,8 +43,12 @@ Si conoce la dirección, puede leer los correos electrónicos.<br/><br/>
 <strong>En principio, todos los correos electrónicos son públicos.<br/>
 Así que no lo use para datos confidenciales.</strong></p>";
 
-$locale['Copyright']  = "<small><a href=\"https://github.com/pfeifferch/disposable-mailbox\"><strong>disposable-mailbox</strong></a> ".$config['versionnumber']."_ES - una bifurcación separada de <a href=\"https://github.com/synox/disposable-mailbox\"><strong>synox</strong></a></small>";
+$locale['success'] = "Exitosamente";
+$locale['copied'] = "Copiado!";
+$locale['show'] = "mostrar";
 
 $locale['ourdomains'] = "Nuestros dominios";
+
+$locale['Copyright']  = "<small><a href=\"https://github.com/pfeifferch/disposable-mailbox\"><strong>disposable-mailbox</strong></a> ".$config['versionnumber']."_ES - una bifurcación separada de <a href=\"https://github.com/synox/disposable-mailbox\"><strong>synox</strong></a></small>";
 
 ?>
