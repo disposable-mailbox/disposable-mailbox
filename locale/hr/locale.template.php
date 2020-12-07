a4f5e016dda3e375ba86f6e3e6927d3d6cb34329
pfeifferch <73090220+pfeifferch@users.noreply.github.com>
2020-12-07 23:51:14 +0100

    Update locale.template.php

--- locale/hr/locale.template.php
+++ locale/hr/locale.template.php
@@ -29,8 +29,8 @@ $locale['EmptyMailbox']  = "<p> Poštanski sandučić je prazan.  Sve dok je ova
 $locale['QuickSummary']  = "Ovo je usluga jednosmjernog poštanskog sandučića.  Tko zna vaše korisničko ime, može čitati vaše e-mailove.  <br/> E-adrese se automatski i nepovratno brišu nakon ". $config ['delete_messages_older_than']."  Dana.";
 $locale['collapse']  = "Više informacija";
 $locale['long-about-1']  = "<p class=\"text-justify\">Ovaj jednokratni poštanski sandučić sačuvat će vaš glavni poštanski sandučić bez neželjene pošte..</p>";
-$locale['long-about-2']  = "<p class=\"text-justify\">W&auml;hlen Sie einfach eine Adresse und verwenden Sie sie auf Websites, denen Sie nicht vertrauen und wo Sie die Private Haupt-E-Mail-Adresse nicht preisgeben wollen. <br/>Sobald Sie fertig sind, können Sie die Mailbox einfach vergessen. <br/> Der ganze Spam bleibt hier wird nicht im Privaten Mailaccount landen.</p>";
-$locale['long-about-3']  = "<p class=\"text-justify\">Sie wählen die Adresse aus, die Sie verwenden m&ouml;chten, und empfangene E-Mails werden automatisch angezeigt. <br/>Es gibt keine Registrierung und keine Passwörter. <br/>Wenn Sie die Adresse kennen, können Sie die E-Mails lesen.<br/><br/>             <strong>Grunds&auml;tzlich sind alle E-Mails &ouml;ffentlich. <br/>Verwenden Sie es also nicht für vertrauliche Daten.</strong></p>";
+$locale['long-about-2']  = "<p class=\"text-justify\">ne vjerujete i ne želite koristiti svoju glavnu adresu e-pošte.Kad završite, jednostavno možete zaboraviti na poštanski sandučić. Sav neželjeni sadržaj ostaje ovdje i ne ispunjava vaš glavni poštanski sandučić.</p>";
+$locale['long-about-3']  = "<p class=\"text-justify\">Odaberite adresu koju želite koristiti i primljena e-pošta će se automatski prikazati.Nema registracije i lozinki.  Ako znate adresu, možete pročitati e-poštu.<strong>U osnovi su sve e-adrese javne.  Stoga ga nemojte koristiti za osjetljive podatke.</strong></p>";
 
 $locale['ourdomains'] = "Pregled dostupnih domena";
 
