c38197d25ed15b1adc25302a14307e5aadd023c8
pfeifferch <73090220+pfeifferch@users.noreply.github.com>
2020-12-08 17:18:01 +0100

    Update locale.template.php

--- locale/en/locale.template.php
+++ locale/en/locale.template.php
@@ -14,9 +14,7 @@ $locale['MailboxReady']  =  "Your disposable mailbox is ready.";
 $locale['TranslationforCopy']  =  "copy";
 
 $locale['ChangeMailUsername']  = "Change address";
-
 $locale['SetToRandom']  = "Open random mailbox";
-
 $locale['UseOwnUsername']  = "or create your own address:";
 
 $locale['TranslationForDomain']  = "Domain";
@@ -24,7 +22,6 @@ $locale['TranslationForDomain']  = "Domain";
 $locale['OpenMailbox']  = "Open mailbox";
 
 $locale['TranslationForDownload']  =  "Download";
-
 $locale['TranslationForDelete']  =  "Delete";
 
 $locale['EmptyMailbox']  = "<p>Emails will appear here automatically. </p>";
@@ -32,24 +29,19 @@ $locale['EmptyMailbox']  = "<p>Emails will appear here automatically. </p>";
 $locale['QuickSummary']  = "This is a disposable mailbox service. <br/>Whoever knows your username, can read your emails.<br/>&nbsp;<br/>Emails will be deleted after ".$config['delete_messages_older_than']."days.";
 
 $locale['collapse']  = "Show Details";
-
 $locale['long-about-1']  = "<p class=\"text-justify\">This disposable mailbox keeps your main mailbox clean from spam.</p>";
-
-$locale['long-about-2']  = "<p class=\"text-justify\">you don't trust and don't  want to use your main email address.<br/>
-Once you are done, you can just forget about the mailbox. <br/>All the spam stays here and does not fill up your main mailbox.</p>";
-
+$locale['long-about-2']  = "<p class=\"text-justify\">you don't trust and don't  want to use your main email address.<br/>Once you are done, you can just forget about the mailbox. <br/>All the spam stays here and does not fill up your main mailbox.</p>";
 $locale['long-about-3']  = "<p class=\"text-justify\">You select the address you want to use and received emails will be displayed automatically. <br/>There is not registration and no passwords. If you know the address, you can read the emails. <br/><strong>Basically, all emails are public. So don't use it for sensitive data.</strong></p>";
-
 
 $locale['ourdomains'] = "Our Domains";
 
 $locale['success'] = "success";
-
 $locale['copied'] = "Copied!";
-
 $locale['show'] = "show";
 
 $locale['adsLocale'] = "Advertisement:";
+
+$locale['imprintanddisclaimer']  = "Legal information - imprint, disclaimer and more...";
 
 $locale['Copyright']  = "<small><a href=\"https://github.com/pfeifferch/disposable-mailbox\"><strong>disposable-mailbox</strong></a> ".$config['versionnumber']."_EN</small>";
 ?>
