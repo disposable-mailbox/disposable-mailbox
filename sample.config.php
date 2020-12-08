5849be6731e2efae93c03cb76f52f171e953506a
pfeifferch <73090220+pfeifferch@users.noreply.github.com>
2020-12-08 23:31:36 +0100

    Update sample.config.php

--- sample.config.php
+++ sample.config.php
@@ -5,6 +5,8 @@ date_default_timezone_set('Europe/Paris');
 // set locale (see supported locales: https://github.com/fightbulc/moment.php#switch-locale)
 // format Date and Time
 $config['locale'] = 'en_US';
+
+/*
 $config['locale_supported_languages'] = array(
 'ar_TN', 'ca_ES', 'zh_CN', 'zh_TW', 'cs_CZ', 
 'da_DK', 'nl_NL', 'en_CA', 'en_GB', 'en_US', 
@@ -13,7 +15,7 @@ $config['locale_supported_languages'] = array(
 'lv_LV', 'pl_PL', 'pt_BR','pt_PT', 'ru_RU', 
 'es_ES', 'sv_SE', 'uk_UA', 'th_TH', 'tr_TR', 
 'vi_VN');
-
+*/
 
 // User Changable Languages
 $config['availablelanguages'] = array(
@@ -67,13 +69,3 @@ $config['blocked_usernames'] = array('root', 'admin', 'administrator', 'hostmast
 
 // Mails are usually show as Text and only if not available as HTML. You can turn this around to prefer HTML over text.
 $config['prefer_plaintext'] = true;
-
-// Impressum and Disclaimer show in "more Info"
-config['imprintanddisclaimer'] =
-"<p>This service is provided for test purposes and free of charge; there is no guarantee of constant availability, in particular not of the accessibility of the domains </p>
-<h5 style=\"text-align: right;\"><a href=\"https://example.com/contact-form \">CONTACT</a></h5>
-<h5 style=\"text-align: right;\"><a href=\"https://example.com/yourimprint\">IMPRINT</a></h5>
-<h5 style=\"text-align: right;\"><a href=\"https://example.com/yourdisclaimer.html\">DISCLAIMER</a></h5>
-<h5 style=\"text-align: right;\"><a href=\"https://example.com/cookiesandmore.info\">DATA PROTECTION</a></h5>
-<h5 style=\"text-align: right;\"><a href=\"https://example.com/ise.php\">REFERENCES</a></h5>	
-</p>";
