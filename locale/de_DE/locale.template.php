94a9e485c5fbabadcb980d7ca15f2ab3e69dc8b2
pfeifferch <73090220+pfeifferch@users.noreply.github.com>
2020-12-13 06:07:19 +0100

    Create locale.template.php

---
+++ locale/de_DE/locale.template.php
@@ -0,0 +1,4 @@
+<?php 
+@include './locale/de/locale.template.php';
+$locale['Copyright']  = "<small><a href=\"https://github.com/pfeifferch/disposable-mailbox\"><strong>disposable-mailbox</strong></a> ".$config['versionnumber']." (DE-DE)</small>";
+?>
