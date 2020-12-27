ddb1a82595f61a4bb1ca4967ec5b46c93a4e166a
pfeifferch <73090220+pfeifferch@users.noreply.github.com>
2020-12-27 13:02:19 +0100

    Update locale.template.php

--- locale/de/locale.template.php
+++ locale/de/locale.template.php
@@ -43,6 +43,6 @@ $locale['show'] = "Anzeigen";
 
 $locale['adsLocale'] = "Werbeanzeige:";
 
-$locale['Copyright']  = "<small><a href=\"https://github.com/pfeifferch/disposable-mailbox\"><strong>disposable-mailbox</strong></a> ".$config['versionnumber']."(DE)</small>";
+$locale['Copyright']  = "<small><a href=\"https://github.com/pfeifferch/disposable-mailbox\"><strong>disposable-mailbox</strong></a> ".$config['versionnumber']." (DE)</small>";
 
 ?>
