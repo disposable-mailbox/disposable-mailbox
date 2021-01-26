1324436daa5ac6ea399c0fdaeed6415cbd7c65bd
pfeifferch <73090220+pfeifferch@users.noreply.github.com>
2021-01-26 21:53:23 +0100

    Create locale.SoMedia.php

---
+++ locale/hr/locale.SoMedia.php
@@ -0,0 +1,27 @@
+<?php 
+$localeSoMePhrases['FollowHeading'] = "prati nas";
+
+$localeSoMePhrases['followFB'] = "Poput nas ";
+$localeSoMePhrases['followTwttr'] = "Pratite nas na ";
+$localeSoMePhrases['PinOnPinterest'] = "Napomena uz ";
+$localeSoMePhrases['followFB'] = "Poput nas ";
+$localeSoMePhrases['followYT'] = "Pretplatite se na nas ";
+$localeSoMePhrases['followInsta'] = "Pratite nas na ";
+
+
+$localeSoMePhrases['CommunityHeading'] = "Razgovarajte / programirajte s nama";
+
+$localeSoMePhrases['devOnGitHub'] = "Doprinosi ";
+$localeSoMePhrases['devOnDocker'] = "Doprinosi ";
+$localeSoMePhrases['chatOnGitter'] = "Pridružite se chatu ";
+$localeSoMePhrases['chatOnWtsApp'] = "Razgovaraj sa mnom putem ";
+
+
+$localeSoMePhrases['ShareHeading'] = "Podijelite ovaj post / stranicu";
+
+$localeSoMePhrases['ShareOnFB'] = "Podijelite ovaj post ";
+$localeSoMePhrases['ShareOnTwttr'] = "Podijelite ovaj post ";
+$localeSoMePhrases['ShareOnInsta'] = "Podijelite ovaj post ";
+$localeSoMePhrases['ShareOnWtsApp'] = "Podijelite ovaj post ";
+
+?>
