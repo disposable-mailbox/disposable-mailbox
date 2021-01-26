3ac6c78ab752a8a5a1f945000eb89401f9c2b5c8
pfeifferch <73090220+pfeifferch@users.noreply.github.com>
2021-01-26 21:59:51 +0100

    Create locale.SoMedia.php

---
+++ locale/nl/locale.SoMedia.php
@@ -0,0 +1,27 @@
+<?php 
+$localeSoMePhrases['FollowHeading'] = "Síguenos";
+
+$localeSoMePhrases['followFB'] = "Como nosotros ";
+$localeSoMePhrases['followTwttr'] = "siga con nosotros ";
+$localeSoMePhrases['PinOnPinterest'] = "Nota con ";
+$localeSoMePhrases['followFB'] = "Como nosotros ";
+$localeSoMePhrases['followYT'] = "Suscríbete a nosotros ";
+$localeSoMePhrases['followInsta'] = "siga con nosotros ";
+
+
+$localeSoMePhrases['CommunityHeading'] = "Habla / programa con nosotros";
+
+$localeSoMePhrases['devOnGitHub'] = "Contribuir a ";
+$localeSoMePhrases['devOnDocker'] = "Contribuir a ";
+$localeSoMePhrases['chatOnGitter'] = "Únete al chat ";
+$localeSoMePhrases['chatOnWtsApp'] = "Chatea conmigo a través de ";
+
+
+$localeSoMePhrases['ShareHeading'] = "Comparte esta publicación / página";
+
+$localeSoMePhrases['ShareOnFB']     = "Compartir esta publicacion ";
+$localeSoMePhrases['ShareOnTwttr']  = "Compartir esta publicacion ";
+$localeSoMePhrases['ShareOnInsta']  = "Compartir esta publicacion ";
+$localeSoMePhrases['ShareOnWtsApp'] = "Compartir esta publicacion ";
+
+?>
