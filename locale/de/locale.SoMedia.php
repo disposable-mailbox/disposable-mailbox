ea0df84aafdd8919ecf38fc2b5f21acb9a1a4ffb
pfeifferch <73090220+pfeifferch@users.noreply.github.com>
2021-01-26 18:28:54 +0100

    Create locale.SoMedia.php

---
+++ locale/de/locale.SoMedia.php
@@ -0,0 +1,27 @@
+<?php 
+$localeSoMePhrases['FollowHeading'] = "DE: FOLLOW US";
+
+$localeSoMePhrases['followFB'] = "Like us on ";
+$localeSoMePhrases['followTwttr'] = "Follow us on ";
+$localeSoMePhrases['PinOnPinterest'] = "Save on ";
+$localeSoMePhrases['followFB'] = "Like us on ";
+$localeSoMePhrases['followYT'] = "Subscribe us on ";
+$localeSoMePhrases['followInsta'] = "Follow us on ";
+
+
+$localeSoMePhrases['CommunityHeading'] = "DE: TALK / CONTRIBUTE WITH US";
+
+$localeSoMePhrases['devOnGitHub'] = "Contribute on ";
+$localeSoMePhrases['devOnDocker'] = "Contribute on ";
+$localeSoMePhrases['chatOnGitter'] = "Join the Chat on ";
+$localeSoMePhrases['chatOnWtsApp'] = "Chat with me on ";
+
+
+$localeSoMePhrases['ShareHeading'] = "DE: SHARE THIS PAGE";
+
+$localeSoMePhrases['ShareOnFB'] = "Share this post on ";
+$localeSoMePhrases['ShareOnTwttr'] = "Share this post on ";
+$localeSoMePhrases['ShareOnInsta'] = "Share this post on ";
+$localeSoMePhrases['ShareOnWtsApp'] = "Share this post on ";
+
+?>
