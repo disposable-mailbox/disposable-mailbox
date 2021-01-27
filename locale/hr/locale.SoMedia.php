e8dcab799a9460c59dd61d7ff26c8fb5a1ea0eaa
pfeifferch <73090220+pfeifferch@users.noreply.github.com>
2021-01-27 13:16:04 +0100

    Update locale.SoMedia.php

--- locale/hr/locale.SoMedia.php
+++ locale/hr/locale.SoMedia.php
@@ -3,10 +3,11 @@ $localeSoMePhrases['FollowHeading'] = "prati nas";
 
 $localeSoMePhrases['followFB'] = "Poput nas ";
 $localeSoMePhrases['followTwttr'] = "Pratite nas na ";
-$localeSoMePhrases['PinOnPinterest'] = "Napomena uz ";
 $localeSoMePhrases['followFB'] = "Poput nas ";
 $localeSoMePhrases['followYT'] = "Pretplatite se na nas ";
 $localeSoMePhrases['followInsta'] = "Pratite nas na ";
+$localeSoMePhrases['followPinterest'] = "Pratite nas na ";
+$localeSoMePhrases['PinOnPinterest'] = "Napomena uz ";
 
 
 $localeSoMePhrases['CommunityHeading'] = "Razgovarajte / programirajte s nama";
@@ -14,6 +15,7 @@ $localeSoMePhrases['CommunityHeading'] = "Razgovarajte / programirajte s nama";
 $localeSoMePhrases['devOnGitHub'] = "Doprinosi ";
 $localeSoMePhrases['devOnDocker'] = "Doprinosi ";
 $localeSoMePhrases['chatOnGitter'] = "Pridružite se chatu ";
+$localeSoMePhrases['chatOnElement'] = "Pridružite se chatu ";
 $localeSoMePhrases['chatOnWtsApp'] = "Razgovaraj sa mnom putem ";
 
 
