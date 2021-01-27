59f4e8579a459aaae5df09e8522d028f46b31e77
pfeifferch <73090220+pfeifferch@users.noreply.github.com>
2021-01-27 20:14:48 +0100

    Update sample.social.usercontent.php

--- sample.social.usercontent.php
+++ sample.social.usercontent.php
@@ -1,9 +1,35 @@
 <?php 
 
-/*
-// Twitter / FB / GitHub / ... Usernames, ...
+$SoMeSettings['UserFB'] = "disposablemailbox";
+$SoMeSettings['UserTwttr'] = "disposablemxbox";
+$SoMeSettings['UserInsta'] = "";
+$SoMeSettings['UserYT'] = "";
+$SoMeSettings['UserPinterest'] = "disposablemxbox";
+$SoMeSettings['PinPinterest'] = "";
+
+$SoMeSettings['UserGitHub'] = "pfeifferch";
+$SoMeSettings['RepoGitHub'] = "disposable-mailbox-ML";
+$SoMeSettings['UserDocker'] = "pfeifferch";
+$SoMeSettings['UserGitter'] = "pfeifferch";
+$SoMeSettings['GitterRoom'] = "disposable-mailbox";
+$SoMeSettings['UserElement'] = "";
+$SoMeSettings['ElementRoom'] = "";
+
+$SoMeSettings['WtsAppID'] = "";
+$SoMeSettings['ShareFB'] = "";
+$SoMeSettings['ShareTwttr'] = "";
+$SoMeSettings['ShareInsta'] = "";
+$SoMeSettings['ShareWtsApp'] = "";
 
 
-*/
+$SoMeSettings['URL.FB'] = "https://fb.me/".SoMeSettings['UserFB'];
+$SoMeSettings['URL.Twttr'] = "https://twitter.com/".SoMeSettings['UserTwttr'];
+$SoMeSettings['URL.Insta'] = "";
+$SoMeSettings['URL.YT'] = "";
+$SoMeSettings['URL.Pin'] = "https://www.pinterest.de/".SoMeSettings['UserPinterest']."/pins/follow/";
+$SoMeSettings['URL.GitHubProfile'] = "https://github.com/".SoMeSettings['UserGitHub'];
+$SoMeSettings['URL.GitHupRepo'] ="https://github.com/".SoMeSettings['UserGitHub']."/".SoMeSettings['RepoGitHub']."/";
+$SoMeSettings['URL.GitterRoom'] = "https://gitter.im/".$SoMeSettings['UserGitter']."/".$SoMeSettings['GitterRoom']; 
+$SoMeSettings['URL.ElementRoom'] = "https://element.im/".SoMeSettings['UserElement']."/".SoMeSettings['ElementRoom'];
 
 ?>
