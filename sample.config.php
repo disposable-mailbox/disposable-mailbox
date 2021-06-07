f52b00644473763821c6329807030af824bc2f99
pfeifferch <73090220+pfeifferch@users.noreply.github.com>
2021-06-07 03:25:57 +0200

    Update sample.config.php

--- sample.config.php
+++ sample.config.php
@@ -6,6 +6,61 @@ date_default_timezone_set('Europe/Paris');
 // format Date and Time
 $config['locale'] = 'en_US';
 
+/*
+$moment_supported_languages = array(
+   "de_DE" =>   "de", 
+   "de_DE" =>   "de_DE", 
+   "de_DE" =>   "de_AT", 
+   "de_DE" =>   "de_CH", 
+   "de_DE" =>   "de_LI", 
+   "de_DE" =>   "de_LT",
+   "en_US" =>   "en", 
+   "en_US" =>	"en_US",
+   "eb_GB" =>	"en_GB",
+   "nl_NL" => 	"nl", 
+   "nl_NL" => 	"nl_NL",
+);
+*/
+/*
+moment Supported languages so far:
+ar_TN Arabic (Tunisia)
+ca_ES Catalan
+zh_CN Chinese
+zh_TW Chinese (traditional)
+cs_CZ Czech
+da_DK Danish
+nl_NL Dutch
+en_CA English (Canada)
+en_GB English (British)
+en_US English (American)
+eo_EO Esperanto
+fa_IR Farsi
+fi_FI Finnish
+fr_FR French (Europe)
+fr_CA French (Canada)
+de_DE German (Germany)
+hu_HU Hungarian
+id_ID Indonesian
+it_IT Italian
+ja_JP Japanese
+kz_KZ Kazakh
+oc_LNC Lengadocian
+lv_LV Latvian (Latviešu)
+pl_PL Polish
+pt_BR Portuguese (Brazil)
+pt_PT Portuguese (Portugal)
+ru_RU Russian (Basic version)
+es_ES Spanish (Europe)
+sv_SE Swedish
+uk_UA Ukrainian
+th_TH Thai
+tr_TR Turkish
+vi_VN Vietnamese
+*/
+
+
+
+
 // User Changable Languages
 $config['availablelanguages'] = array(
 "English" => "en",
